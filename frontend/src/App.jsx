62bb9570dd76234f72e3a515ac9143e71f2a6a5a
Vikas Maury <mauryvikas12345@gmail.com>
2025-03-02 20:20:48 +0530

    added minikube deployment and other k8s objects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from 'axios'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-    const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
-    const fetchText = async () => {
-      try {
-        const response = await axios.get('http://fastapi-app-service/')
-        setText(response.data)
-      } catch (error) {
-        console.error('Error fetching text:', error)
-      }
+  const fetchText = async () => {
+    try {
+      const apiEndpoint = import.meta.env.VITE_FASTAPI_ENDPOINT || "/api/";
+      console.log("API Endpoint:", apiEndpoint);
+
+      const response = await axios.get(apiEndpoint);
+      console.log("Response:", response); // Debugging log
+      setText(response.data);
+    } catch (error) {
+      console.error("Error fetching text:", error);
     }
+  };
 
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
       <button
         onClick={fetchText}
         className="px-4 py-2 mb-4 text-white bg-blue-600 rounded hover:bg-blue-700"
@@ -25,8 +29,8 @@ function App() {
         Fetch Text from API
       </button>
       {text && <h1 className="text-3xl font-bold text-white">{text}</h1>}
-      </div>
-    )
+    </div>
+  );
 }
 
-export default App
+export default App;
